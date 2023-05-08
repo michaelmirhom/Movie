c5c2125123a6fc1313d4565ce850e1998d17c4bb
michaelmirhom <micharby73@gmail.com>
2023-05-08 00:54:26 -0500

    Fix bug in search results display

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const searchInput = document.getElementById('search-input');
 const movieContainer = document.getElementById('movie-container');
 const clearSearchButton = document.getElementById('clear-search');
 const showFavoritesButton = document.getElementById('show-favorites');
+
 searchInput.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
         const searchTerm = event.target.value;
@@ -12,32 +13,34 @@ searchInput.addEventListener('keyup', (event) => {
 clearSearchButton.addEventListener('click', clearSearchInput);
 showFavoritesButton.addEventListener('click', showFavoriteMovies);
 
-// Function to clear the search input field
 function clearSearchInput() {
     searchInput.value = '';
 }
-let favorites = localStorage.getItem('favorites');
-if (favorites) {
-    favorites = JSON.parse(favorites);
-    // Fetch movie data for each favorite movie ID
-    const moviePromises = favorites.map(async (id) => {
-        const response = await fetch(`http://localhost:3000/movies/${id}`);
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        } else {
-            console.error(`Error fetching movie with ID ${id}: ${response.statusText}`);
-            return null;
-        }
-    });
-     // Resolve all movie promises and filter out null values
-     const movies = (await Promise.all(moviePromises)).filter(movie => movie !== null);
-     // Display favorite movies
-     displayMovies(movies, true);
- } else {
-     movieContainer.innerHTML = '<p>No favorite movies found.</p>';
- }
- async function fetchMovies(searchTerm) {
+
+async function showFavoriteMovies() {
+    let favorites = localStorage.getItem('favorites');
+    if (favorites) {
+        favorites = JSON.parse(favorites);
+        const moviePromises = favorites.map(async (id) => {
+            const response = await fetch(`http://localhost:3000/movies/${id}`);
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                console.error(`Error fetching movie with ID ${id}: ${response.statusText}`);
+                return null;
+            }
+        });
+
+        const movies = (await Promise.all(moviePromises)).filter(movie => movie !== null);
+        displayMovies(movies, true);
+    } else {
+        movieContainer.innerHTML = '<p>No favorite movies found.</p>';
+    }
+}
+
+
+async function fetchMovies(searchTerm) {
     const response = await fetch(`http://localhost:3000/movies?q=${searchTerm}`);
     const data = await response.json();
     if (data.length > 0) {
@@ -46,6 +49,7 @@ if (favorites) {
         movieContainer.innerHTML = '<p>No movies found.</p>';
     }
 }
+
 function displayMovies(movies, isFavorite) {
     movieContainer.innerHTML = '';
     movies.forEach((movie) => {
@@ -60,23 +64,24 @@ function displayMovies(movies, isFavorite) {
         `;
         const saveFavoriteButton = movieCard.querySelector('.save-favorite');
         const removeFavoriteButton = movieCard.querySelector('.remove-favorite');
+        
         saveFavoriteButton.addEventListener('click', () => {
             saveToFavorites(movie.id);
             saveFavoriteButton.hidden = true;
             removeFavoriteButton.hidden = false;
         });
+        
         removeFavoriteButton.addEventListener('click', () => {
             removeFromFavorites(movie.id);
             saveFavoriteButton.hidden = false;
             removeFavoriteButton.hidden = true;
         });
 
-     
         movieContainer.appendChild(movieCard);
     });
 }
+
 function saveToFavorites(id) {
-    // Get the current list of favorites from local storage
     let favorites = localStorage.getItem('favorites');
 
     if (favorites) {
@@ -84,18 +89,18 @@ function saveToFavorites(id) {
     } else {
         favorites = [];
     }
+
     if (!favorites.includes(id)) {
         favorites.push(id);
         localStorage.setItem('favorites', JSON.stringify(favorites));
     }
 }
+
 function removeFromFavorites(id) {
-    // Get the current list of favorites from local storage
     let favorites = localStorage.getItem('favorites');
 
     if (favorites) {
         favorites = JSON.parse(favorites);
-        // Find the index of the movie ID in the favorites list
         const index = favorites.indexOf(id);
         if (index !== -1) {
             favorites.splice(index, 1);
