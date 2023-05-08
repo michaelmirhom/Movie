74ec94378e3d7dc4c01432c1e590867e407cb63c
michaelmirhom <micharby73@gmail.com>
2023-05-07 22:29:22 -0500

    Add event listeners to handle user input and button clicks

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ const searchInput = document.getElementById('search-input');
 const movieContainer = document.getElementById('movie-container');
 const clearSearchButton = document.getElementById('clear-search');
 const showFavoritesButton = document.getElementById('show-favorites');
+searchInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        const searchTerm = event.target.value;
+        fetchMovies(searchTerm);
+    }
+});
+
+clearSearchButton.addEventListener('click', clearSearchInput);
+showFavoritesButton.addEventListener('click', showFavoriteMovies);
+
+// Function to clear the search input field
+function clearSearchInput() {
+    searchInput.value = '';
+}
