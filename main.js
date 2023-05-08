735dce29118aba34e8f64af55bddae5b9a848429
michaelmirhom <micharby73@gmail.com>
2023-05-07 22:27:54 -0500

    Get references to HTML elements

---
+++ main.js
@@ -0,0 +1,4 @@
+const searchInput = document.getElementById('search-input');
+const movieContainer = document.getElementById('movie-container');
+const clearSearchButton = document.getElementById('clear-search');
+const showFavoritesButton = document.getElementById('show-favorites');
